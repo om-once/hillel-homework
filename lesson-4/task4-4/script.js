140b1e9edaa95ae8425724c66397df5816a2e017
OM_Once <minenkogt@gmail.com>
2024-10-03 09:16:46 +0300

    task 4.4 is done

--- lesson-4/task4-4/script.js
+++ lesson-4/task4-4/script.js
@@ -1,68 +1,27 @@
-const birthYear = prompt('Введіть рік народження');
-const cityLive = prompt('Введіть місто в якому живете');
-const loveSport = prompt('Ваш улюблений вид спорту');
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
 
-const capitals = [
-	{
-		capital: 'Лондон',
-		country: 'Великої Британії',
-	},
-	{
-		capital: 'Київ',
-		country: 'України',
-	},
-	{
-		capital: 'Вашингтон',
-		country: 'США',
-	},
-];
+// if (numOrStr === null) {
+//     console.log('ви скасували')
+// } else if (numOrStr.trim() === '') {
+//     console.log('Empty String');
+// } else if (isNaN(+numOrStr)) {
+//     console.log(' number is Ba_NaN')
+// } else {
+//     console.log('OK!')
+// }
 
-const champions = [
-	{
-		sport: 'Бокс',
-		champion: 'Олександр Усик',
-	},
-	{
-		sport: 'Футбол',
-		champion: 'Андрій Шевченко',
-	},
-	{
-		sport: 'Боротьба',
-		champion: 'Жан Беленюк',
-	},
-];
-
-if (birthYear.trim() === '' || birthYear === null || !isFinite(birthYear)) {
-	alert(
-		'Шкода що ви не захотіли ввести свою дату народження або ввели її в неправильному форматі',
-	);
-} else if (cityLive.trim() === '' || cityLive === null) {
-	alert('Шкода що ви не захотіли ввести місто в якому живете');
-} else if (loveSport.trim() === '' || loveSport === null) {
-	alert('Шкода що ви не захотіли ввести улюблений вид спорту');
-} else {
-	let yearsOld = 2024 - Number(birthYear);
-	let cheсkCity = capitals.find(item => {
-		return item.capital.toLowerCase() === cityLive.toLowerCase();
-	});
-	let cheсkChampions = champions.find(item => {
-		return item.sport.toLowerCase() === loveSport.toLowerCase();
-	});
-	if (cheсkCity && !cheсkChampions) {
-		alert(
-			`Твій вік ${yearsOld}. Ти живеш у столиці - ${cheсkCity.country}. Твій улюблений вид спорту - ${loveSport}`,
-		);
-	} else if (!cheсkCity && cheсkChampions) {
-		alert(
-			`Твій вік ${yearsOld}. Ти живеш у місті - ${cityLive}. Твій улюблений вид спорту - ${loveSport}. Круто! Хочеш бути як ${cheсkChampions.champion}?`,
-		);
-	} else if (cheсkCity && cheсkChampions) {
-		alert(
-			`Твій вік ${yearsOld}. Ти живеш у столиці ${cheсkCity.country}. Твій улюблений вид спорту - ${loveSport}. Круто! Хочеш бути як ${cheсkChampions.champion}?`,
-		);
-	} else {
-		alert(
-			`Твій вік ${yearsOld}. Ти живеш у місті - ${cityLive}. Твій улюблений вид спорту - ${loveSport}`,
-		);
-	}
+switch (true) {
+	case numOrStr === null:
+		console.log('ви скасували');
+		break;
+	case numOrStr.trim() === '':
+		console.log('Empty String');
+		break;
+	case isNaN(+numOrStr):
+		console.log(' number is Ba_NaN');
+		break;
+	default:
+		console.log('OK!');
+		break;
 }
